a07b885d6432cf2c000a4015046d656ae2f0483b
Chayanit11 <chayanithawea185@gmail.com>
2023-08-31 18:03:20 +0700

    yeah this is first commit

---
+++ lib/models/config.dart
@@ -0,0 +1,7 @@
+import 'users.dart';
+
+class Configure {
+  static const server = "10.116.3.58:3000";
+  static Users login = Users();
+  static List<String> gender = ["None", "Male", "Female"];
+}
---
+++ lib/screen/sidemenu.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_12/screen/home.dart';
+import 'package:flutter_application_12/screen/login.dart';
+
+import 'package:flutter_application_12/models/config.dart';
+import 'package:flutter_application_12/models/users.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String accountName = "N/A";
+    String accountEmail = "N/A";
+    String accountUrl =
+        "https://images.pexels.com/photos/1741205/pexels-photo-1741205.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
+    Users user = Configure.login;
+    //print(user.toJson().toString());
+    if (user.id != null) {
+      accountName = user.fullname!;
+      accountEmail = user.email!;
+    }
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text(accountName),
+            accountEmail: Text(accountEmail),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(accountUrl),
+              backgroundColor: Colors.white,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pushNamed(context, Home.routeName);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.lock),
+            title: Text('Login'),
+            onTap: () {
+              Navigator.pushNamed(context, Login.routeName);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
